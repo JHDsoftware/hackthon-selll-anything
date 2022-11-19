3386993459f485428d4dfc8b09854096b4505771
Haodong Ju <juhaodong@gmail.com>
2022-11-19 07:49:23 +0100

    抽出内部组件

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -20,46 +20,7 @@
     <v-main style="background: #f0f0f0;min-height: calc(100vh)">
       <div class="pa-6">
         <div style="display: grid;grid-template-columns: repeat(auto-fit,minmax(180px,1fr));grid-gap: 12px">
-          <v-card style="user-select: none;border-radius: 16px" v-for="t in 12" :key="t" color="white"
-                  elevation="0">
-            <v-img :aspect-ratio="1" src="https://random.imagecdn.app/500/500">
-              <template #placeholder>
-                <div style="width: 100%;height: 120px;"
-                     class="d-flex align-center justify-center">
-                  <v-progress-circular indeterminate/>
-                </div>
-              </template>
-            </v-img>
-            <div class="pa-2 px-4">
-              <div class="text-caption">
-                <span class="success--text text--darken-3 font-weight-medium">Sales</span>
-                by
-                <v-avatar size="14" class="ml-1">
-                  <v-img :src="'https://api.multiavatar.com/'+t+'.svg'"></v-img>
-                </v-avatar>
-              </div>
-
-              <div class="d-flex align-center">
-                <div class="text-body-1">
-                  Real Thing with long name
-                </div>
-              </div>
-              <div class="mt-1 font-weight-medium">
-                {{ 123 | priceDisplay }}
-              </div>
-              <div class="text-caption mt-n1 d-flex align-center">
-                <div>
-                  123 in stock
-                </div>
-
-
-                <v-spacer></v-spacer>
-
-              </div>
-
-
-            </div>
-          </v-card>
+          <order-card v-for="t in 12" :key="t"></order-card>
         </div>
       </div>
     </v-main>
@@ -70,10 +31,11 @@
 import LogoDisplay from "@/views/widgets/LogoDisplay";
 import VersionDisplay from "@/views/widgets/VersionDisplay";
 import {getCurrentUserId} from "@/dataLayer/service/firebase/user";
+import OrderCard from "@/views/widgets/items/OrderCard";
 
 export default {
   name: "HomePage",
-  components: {VersionDisplay, LogoDisplay},
+  components: {OrderCard, VersionDisplay, LogoDisplay},
   data: function () {
     return {
       userId: getCurrentUserId()
---
+++ src/views/widgets/items/OrderCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card style="user-select: none;border-radius: 16px" color="white"
+          elevation="0">
+    <v-img :aspect-ratio="1" src="https://random.imagecdn.app/500/500">
+      <template #placeholder>
+        <div style="width: 100%;height: 120px;"
+             class="d-flex align-center justify-center">
+          <v-progress-circular indeterminate/>
+        </div>
+      </template>
+    </v-img>
+    <div class="pa-2 px-4">
+      <div class="text-caption">
+        <span class="success--text text--darken-3 font-weight-medium">Sales</span>
+        by
+        <v-avatar size="14" class="ml-1">
+          <v-img :src="'https://api.multiavatar.com/'+t+'.svg'"></v-img>
+        </v-avatar>
+      </div>
+
+      <div class="d-flex align-center">
+        <div class="text-body-1">
+          Real Thing with long name
+        </div>
+      </div>
+      <div class="mt-1 font-weight-medium">
+        {{ 123 | priceDisplay }}
+      </div>
+      <div class="text-caption mt-n1 d-flex align-center">
+        <div>
+          123 in stock
+        </div>
+
+
+        <v-spacer></v-spacer>
+
+      </div>
+
+
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "OrderCard"
+}
+</script>
+
+<style scoped>
+
+</style>
