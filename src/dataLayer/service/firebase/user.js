fbfabf63d64316135759f03fcb4be6bff54862bc
Xiyue <ge32kit@mytum.de>
2022-11-19 12:40:45 +0100

    merge user.js conflict

--- src/dataLayer/service/firebase/user.js
+++ src/dataLayer/service/firebase/user.js
@@ -1,8 +1,8 @@
-import {doc, setDoc} from 'firebase/firestore'
+import { doc, setDoc} from 'firebase/firestore'
 import {FireBaseAuth, FireBaseStore} from '@/plugins/google-fire-base'
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, onValue, update } from "firebase/database";
 
-const userDBPath = 'users'
+const userDBPath = 'user'
 
 /**
  * 注意！！请只在FireBase提供的Hook中调用本函数
@@ -19,20 +19,85 @@ export function getCurrentUserId() {
     return FireBaseAuth.currentUser?.uid
 }
 
-<<<<<<< HEAD
 export function getCurrentUser() {
     return FireBaseAuth.currentUser
 }
 
-
-=======
-export function writeUserData(userId, name, email, psw) {
+/**
+ * 添加用户
+ * @param user_id
+ * @param displayName
+ * @param email
+ * @param password
+ * @param balance
+ * @param timestamp
+ * @return 
+ */
+export function writeUserData(name, email, psw, balance) {
   const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
+  const newUserId = push(child(ref(db), 'user')).key;
+  set(ref(db, 'user/' + newUserId), {
+    user_id: newUserId,
+    displayName: name,
     email: email,
     password: psw,
+    balance: balance,
+    timestamp: Date.now(),
   });
 }
 
->>>>>>> 008d456 (add firebase)
+  /**
+ * 删除user
+ * @param userId
+ * @return 
+ */
+   export function removeItem(userId) {
+    const db = getDatabase();
+    remove(ref(db, 'user/' + userId));
+  }
+
+/**
+ * 查询用户
+ * @param user_id
+ * @return {Promise<void>}
+ * ???auth
+ */
+ export async function getLoggedInUser(userId) {
+    const db = getDatabase();
+    const snapshot = await get(ref(db, '/user/' + userId))
+    user = snapshot.val();
+    return user;
+}
+
+/**
+ * 更新用户余额
+ * @param user_id
+ * @param displayName
+ * @param email
+ * @param password
+ * @param balance
+ * @param timestamp
+ * @return 
+ */
+ export function updateUserBalance(userId, balance) {
+    const db = getDatabase();
+    
+    onValue(ref(db, '/user/' + userId), (snapshot) => {
+        user = {
+            user_id: userId,
+            email: snapshot.val().email,
+            password: snapshot.val().password,
+            displayName: snapshot.val().displayName,
+            timestamp: snapshot.val().timestamp,
+            balance: balance,
+        }
+    }, {
+        onlyOnce: true
+    });
+    
+    const updates = {};
+    updates['/user/' + userId] = user;
+    return update(ref(db), updates);
+  }
+
+
