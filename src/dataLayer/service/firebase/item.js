12061b8072bf4bfc52b7853a25357a023371b87c
Xiyue <ge32kit@mytum.de>
2022-11-19 14:30:25 +0100

    update item

--- src/dataLayer/service/firebase/item.js
+++ src/dataLayer/service/firebase/item.js
@@ -1,4 +1,5 @@
-import {child, get, getDatabase, push, ref, remove, set} from "firebase/database";
+import { collection, getDoc, getDocs, doc, setDoc, deleteDoc, serverTimestamp, query, where } from "firebase/firestore";
+import {GlobalDB} from "@/dataLayer/service/firebase/database";
 
 /**
  * 添加item
@@ -8,17 +9,22 @@ import {child, get, getDatabase, push, ref, remove, set} from "firebase/database
  * @param tagId(list)
  * @return
  */
-export function addItem(itemName, desc, imageUrl, tagId) {
-    const db = getDatabase();
-    const newItemId = push(child(ref(db), 'item')).key;
-    set(ref(db, 'item/' + newItemId), {
-        item_id: newItemId,
-        item_name: itemName,
-        description: desc,
-        imageUrl: imageUrl,
-        tag_id: tagId,
-        timestamp: Date.now(),
-    });
+export async function addItem(itemName, desc, imageUrl, tagId) {
+    try {
+        const newItemId = doc(collection(GlobalDB, "item"));
+
+        await setDoc(newItemId,{
+            item_id: newItemId,
+            item_name: itemName,
+            description: desc,
+            imageUrl: imageUrl,
+            tag_id: tagId,
+            timestamp: serverTimestamp(),
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 }
 
 /**
@@ -26,28 +32,57 @@ export function addItem(itemName, desc, imageUrl, tagId) {
  * @param itemId
  * @return
  */
-export function removeItem(itemId) {
-    const db = getDatabase();
-    remove(ref(db, 'item/' + itemId));
+export async function removeItem(itemId) {
+
+    await deleteDoc(doc(GlobalDB, "item", itemId));
+
 }
 
 /**
- * 查询一个item
+ * 查询items list
+ * @return {Promise<void>}
+ */
+export async function getItems() {
+    const querySnapshot = await getDocs(collection(GlobalDB, "item"));
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+
+    return querySnapshot;
+}
+
+/**
+ * 查询单个item
  * @param itemId
  * @return {Promise<void>}
  */
-export async function getItemOne(itemId) {
-    const db = getDatabase();
-    const snapshot = await get(ref(db, '/item/' + itemId))
-    return snapshot.val();
+export async function getOneItem(itemId) {
+    const docRef = doc(GlobalDB, "item", itemId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+    return docSnap;
 }
 
 /**
- * 查询多个item
+ * 查询单个item by tag
  * @param tagId
  * @return {Promise<void>}
  */
 export async function getItemsByTag(tagId) {
-    const db = getDatabase();
-    return db.collection('item').where('tag_id', 'array-conctains', tagId).get();
+    const q = query(collection(GlobalDB, "item"), where("tag_id", "==", tagId));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+    return querySnapshot;
 }
+
+
